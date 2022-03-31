ade030fdf2bfb935b40c8e2f15ef3d28dd1d43de
jedda jihene <jeddajihene1@gmail.com>
2022-03-31 18:29:41 +0100

    ws

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 const app = express();
 connectDb();
 app.use(express.json());
-app.use("/api/contact", contactRoutes);
+app.use("/api/contacts", contactRoutes);
 app.listen(process.env.PORT, (err) =>
   console.log(`my server is runing ${process.env.PORT}`)
 );
